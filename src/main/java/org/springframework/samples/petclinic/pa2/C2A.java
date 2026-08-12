01bcc7f22257e7206cec6423f59f0df8e3a149f8
Ying Wang <yingwang@yingqwangVTRWM.local>
2026-08-13 01:19:36 +0800

    test(ds): cross-package import cycle (expect DS alert)

---
+++ src/main/java/org/springframework/samples/petclinic/pa2/C2A.java
@@ -0,0 +1,3 @@
+package org.springframework.samples.petclinic.pa2;
+import org.springframework.samples.petclinic.pb2.C2B;
+public class C2A { }
---
+++ src/main/java/org/springframework/samples/petclinic/pb2/C2B.java
@@ -0,0 +1,3 @@
+package org.springframework.samples.petclinic.pb2;
+import org.springframework.samples.petclinic.pa2.C2A;
+public class C2B { }
