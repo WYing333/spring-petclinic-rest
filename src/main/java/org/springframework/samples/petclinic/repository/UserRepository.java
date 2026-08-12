daba04424080411666d23ac825483ae9425e9c57
Ying Wang <yingwang@yingqwangVTRWM.local>
2026-08-13 01:20:41 +0800

    test(bc): repository imports controller (expect BC violation)

--- src/main/java/org/springframework/samples/petclinic/repository/UserRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/UserRepository.java
@@ -1,4 +1,5 @@
 package org.springframework.samples.petclinic.repository;
+import org.springframework.samples.petclinic.rest.controller.v1.VetRestControllerV1;
 
 import org.springframework.dao.DataAccessException;
 import org.springframework.samples.petclinic.model.User;
